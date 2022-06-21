94af30ddc14d7c0a9e43e0bebfac94258d6669af
Dave Wysochanski <dwysocha@redhat.com>
2022-06-21 15:08:20 -0400

    NFS: Convert buffered read paths to use netfs when fscache is enabled
    Convert the NFS buffered read code paths to corresponding netfs APIs,
    but only when fscache is configured and enabled.
    
    The netfs API defines struct netfs_request_ops which must be filled
    in by the network filesystem.  For NFS, we only need to define 5 of
    the functions, the main one being the issue_read() function.
    The issue_read() function is called by the netfs layer when a read
    cannot be fulfilled locally, and must be sent to the server (either
    the cache is not active, or it is active but the data is not available).
    Once the read from the server is complete, netfs requires a call to
    netfs_subreq_terminated() which conveys either how many bytes were read
    successfully, or an error.  Note that issue_read() is called with a
    structure, netfs_io_subrequest, which defines the IO requested, and
    contains a start and a length (both in bytes), and assumes the underlying
    netfs will return a either an error on the whole region, or the number
    of bytes successfully read.
    
    The NFS IO path is page based and the main APIs are the pgio APIs defined
    in pagelist.c.  For the pgio APIs, there is no way for the caller to
    know how many RPCs will be sent and how the pages will be broken up
    into underlying RPCs, each of which will have their own completion and
    return code.  In contrast, netfs is subrequest based, a single
    subrequest may contain multiple pages, and a single subrequest is
    initiated with issue_read() and terminated with netfs_subreq_terminated().
    Thus, to utilze the netfs APIs, NFS needs some way to accommodate
    the netfs API requirement on the single response to the whole
    subrequest, while also minimizing disruptive changes to the NFS
    pgio layer.
    
    The approach taken with this patch is to allocate a small structure
    for each nfs_netfs_issue_read() call, store the final error and number
    of bytes successfully transferred in the structure, and update these values
    as each RPC completes.  The refcount on the structure is used as a marker
    for the last RPC completion, is incremented in nfs_netfs_read_initiate(),
    and decremented inside nfs_netfs_read_completion(), when a nfs_pgio_header
    contains a valid pointer to the data.  On the final put (which signals
    the final outstanding RPC is complete) in nfs_netfs_read_completion(),
    call netfs_subreq_terminated() with either the final error value (if
    one or more READs complete with an error) or the number of bytes
    successfully transferred (if all RPCs complete successfully).  Note
    that when all RPCs complete successfully, the number of bytes transferred
    is capped to the length of the subrequest.  Capping the transferred length
    to the subrequest length prevents "Subreq overread" warnings from netfs.
    This is due to the "aligned_len" in nfs_pageio_add_page(), and the
    corner case where NFS requests a full page at the end of the file,
    even when i_size reflects only a partial page (NFS overread).
    
    Signed-off-by: Dave Wysochanski <dwysocha@redhat.com>

--- fs/nfs/read.c
+++ fs/nfs/read.c
@@ -30,7 +30,7 @@
 
 #define NFSDBG_FACILITY		NFSDBG_PAGECACHE
 
-static const struct nfs_pgio_completion_ops nfs_async_read_completion_ops;
+const struct nfs_pgio_completion_ops nfs_async_read_completion_ops;
 static const struct nfs_rw_ops nfs_rw_read_ops;
 
 static struct kmem_cache *nfs_rdata_cachep;
@@ -74,7 +74,7 @@ void nfs_pageio_init_read(struct nfs_pageio_descriptor *pgio,
 }
 EXPORT_SYMBOL_GPL(nfs_pageio_init_read);
 
-static void nfs_pageio_complete_read(struct nfs_pageio_descriptor *pgio)
+void nfs_pageio_complete_read(struct nfs_pageio_descriptor *pgio)
 {
 	struct nfs_pgio_mirror *pgm;
 	unsigned long npages;
@@ -110,20 +110,13 @@ EXPORT_SYMBOL_GPL(nfs_pageio_reset_read_mds);
 
 static void nfs_readpage_release(struct nfs_page *req, int error)
 {
-	struct inode *inode = d_inode(nfs_req_openctx(req)->dentry);
 	struct page *page = req->wb_page;
-
-	dprintk("NFS: read done (%s/%llu %d@%lld)\n", inode->i_sb->s_id,
-		(unsigned long long)NFS_FILEID(inode), req->wb_bytes,
-		(long long)req_offset(req));
 
 	if (nfs_error_is_fatal_on_server(error) && error != -ETIMEDOUT)
 		SetPageError(page);
-	if (nfs_page_group_sync_on_bit(req, PG_UNLOCKPAGE)) {
-		if (PageUptodate(page))
-			nfs_fscache_write_page(inode, page);
-		unlock_page(page);
-	}
+	if (nfs_page_group_sync_on_bit(req, PG_UNLOCKPAGE))
+		nfs_netfs_readpage_release(req);
+
 	nfs_release_request(req);
 }
 
@@ -177,6 +170,8 @@ static void nfs_read_completion(struct nfs_pgio_header *hdr)
 		nfs_list_remove_request(req);
 		nfs_readpage_release(req, error);
 	}
+	nfs_netfs_read_completion(hdr);
+
 out:
 	hdr->release(hdr);
 }
@@ -187,6 +182,7 @@ static void nfs_initiate_read(struct nfs_pgio_header *hdr,
 			      struct rpc_task_setup *task_setup_data, int how)
 {
 	rpc_ops->read_setup(hdr, msg);
+	nfs_netfs_initiate_read(hdr);
 	trace_nfs_initiate_read(hdr);
 }
 
@@ -202,7 +198,7 @@ nfs_async_read_error(struct list_head *head, int error)
 	}
 }
 
-static const struct nfs_pgio_completion_ops nfs_async_read_completion_ops = {
+const struct nfs_pgio_completion_ops nfs_async_read_completion_ops = {
 	.error_cleanup = nfs_async_read_error,
 	.completion = nfs_read_completion,
 };
@@ -277,7 +273,7 @@ static void nfs_readpage_result(struct rpc_task *task,
 		nfs_readpage_retry(task, hdr);
 }
 
-static int
+int
 nfs_read_add_page(struct nfs_pageio_descriptor *pgio,
 		    struct nfs_open_context *ctx,
 		    struct page *page)
@@ -294,12 +290,6 @@ nfs_read_add_page(struct nfs_pageio_descriptor *pgio,
 
 	aligned_len = min_t(unsigned int, ALIGN(len, rsize), PAGE_SIZE);
 
-	if (!IS_SYNC(page->mapping->host)) {
-		error = nfs_fscache_read_page(page->mapping->host, page);
-		if (error == 0)
-			goto out_unlock;
-	}
-
 	new = nfs_create_request(ctx, page, 0, aligned_len);
 	if (IS_ERR(new))
 		goto out_error;
@@ -315,8 +305,6 @@ nfs_read_add_page(struct nfs_pageio_descriptor *pgio,
 	return 0;
 out_error:
 	error = PTR_ERR(new);
-out_unlock:
-	unlock_page(page);
 out:
 	return error;
 }
@@ -355,6 +343,10 @@ int nfs_read_folio(struct file *file, struct folio *folio)
 	if (NFS_STALE(inode))
 		goto out_unlock;
 
+	ret = nfs_netfs_read_folio(file, folio);
+	if (!ret)
+		goto out;
+
 	if (file == NULL) {
 		ret = -EBADF;
 		ctx = nfs_find_open_context(inode, NULL, FMODE_READ);
@@ -368,8 +360,10 @@ int nfs_read_folio(struct file *file, struct folio *folio)
 			     &nfs_async_read_completion_ops);
 
 	ret = nfs_read_add_page(&pgio, ctx, page);
-	if (ret)
-		goto out;
+	if (ret) {
+		put_nfs_open_context(ctx);
+		goto out_unlock;
+	}
 
 	nfs_pageio_complete_read(&pgio);
 	ret = pgio.pg_error < 0 ? pgio.pg_error : 0;
@@ -378,12 +372,12 @@ int nfs_read_folio(struct file *file, struct folio *folio)
 		if (!PageUptodate(page) && !ret)
 			ret = xchg(&ctx->error, 0);
 	}
-out:
 	put_nfs_open_context(ctx);
-	trace_nfs_aop_readpage_done(inode, page, ret);
-	return ret;
+	goto out;
+
 out_unlock:
 	unlock_page(page);
+out:
 	trace_nfs_aop_readpage_done(inode, page, ret);
 	return ret;
 }
@@ -403,6 +397,10 @@ void nfs_readahead(struct readahead_control *ractl)
 
 	ret = -ESTALE;
 	if (NFS_STALE(inode))
+		goto out;
+
+	ret = nfs_netfs_readahead(ractl);
+	if (!ret)
 		goto out;
 
 	if (file == NULL) {
